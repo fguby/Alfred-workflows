3d5bc7a314b283653a4386ce7c2b0caac3da2219
fguby <fguby1995@mail.com>
2019-05-29 14:40:00 +0800

    Output the XML information to the console

---
+++ Workflow/AlfredWorkflow.go
@@ -0,0 +1,55 @@
+package Workflow
+
+import (
+	"fmt"
+	"strings"
+)
+
+type AlfredWorkflow struct {
+	Valid    string
+	Title    string
+	Subtitle string
+	icon     string
+}
+
+type Alfreds struct {
+	Arrs []AlfredWorkflow
+}
+
+func New() *Alfreds {
+	return &Alfreds{}
+}
+
+func (a *Alfreds) Add(valid, title, subtitle, icon string) {
+	alfred := AlfredWorkflow{
+		Valid:    valid,
+		Title:    title,
+		Subtitle: subtitle,
+		icon:     icon,
+	}
+	a.Arrs = append(a.Arrs, alfred)
+
+}
+
+func (a *Alfreds) SendFeedback() {
+	var build strings.Builder
+	build.WriteString("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n")
+	build.WriteString("<items>\n")
+	for _, value := range a.Arrs {
+		build.WriteString("<item valid=\"")
+		build.WriteString(value.Valid)
+		build.WriteString("\">\n")
+		build.WriteString("<title>")
+		build.WriteString(value.Title)
+		build.WriteString("<title>\n")
+		build.WriteString("<subtitle>")
+		build.WriteString(value.Subtitle)
+		build.WriteString("</subtitle>\n")
+		build.WriteString("<icon>")
+		build.WriteString(value.icon)
+		build.WriteString("</icon>\n")
+		build.WriteString("</item>\n")
+	}
+	build.WriteString("</items>\n")
+	fmt.Print(build.String())
+}
